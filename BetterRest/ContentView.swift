007a5ee061b571812f2997d3890c48286190e738
metalnodeug <metalnodeug@gmail.com>
2019-10-20 14:59:07 +0200

    Building basics layout

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -9,8 +9,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wakeUp = Date()
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack{
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+            }
+        }
     }
 }
 
